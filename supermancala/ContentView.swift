160487518759faeb1640ab62cc36d244c1bc9817
hank <taiwanhank0908@gmail.com>
2023-04-16 16:28:38 +0800

    Initial Commit

---
+++ supermancala/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  supermancala
+//
+//  Created by User19 on 2023/4/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ supermancala/supermancalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  supermancalaApp.swift
+//  supermancala
+//
+//  Created by User19 on 2023/4/16.
+//
+
+import SwiftUI
+
+@main
+struct supermancalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
